d0b6495ba13e9a9db3b4be4e2149c2f2b5760f52
ItsMee04 <meemeeits@gmail.com>
2026-04-04 18:42:32 +0700

    Fitur: update version dan role

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="app-version" content="{{ config('app.version') }}">
     <title>Trifecta Solutions</title>
 
